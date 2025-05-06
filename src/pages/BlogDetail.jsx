1b07d2cf443cf4aecef22eb2d70c18db8a480564
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-05-06 11:56:42 +0000

    update

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -10,10 +10,7 @@ const BlogDetail = () => {
         const fetchPost = async () => {
             try {
                 const response = await api.get(`/blog/${id}`);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
+                const data = await response.data;
                 setPost(data);
             } catch (error) {
                 console.error('Error fetching post:', error);
